202644395890519d0922ff8dccb8f364404ac494
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2023-02-02 08:49:34 -0400

    Add more general styling.

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -6,45 +6,45 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
       <div className="testimonials-cards">
         <div className="testimonials-card">
-          <div>
+          <div className="testimonials-left">
             <p>Rating</p>
             <img src="/assets/logo2.png" width="50" alt="" />
             <p>Review next</p>
           </div>
-          <div>
+          <div className="testimonials-right">
             <p>Name</p>
           </div>
         </div>
 
         <div className="testimonials-card">
-          <div>
+          <div className="testimonials-left">
             <p>Rating</p>
             <img src="/assets/logo2.png" width="50" alt="" />
             <p>Review next</p>
           </div>
-          <div>
+          <div className="testimonials-right">
             <p>Name</p>
           </div>
         </div>
 
         <div className="testimonials-card">
-          <div>
+          <div className="testimonials-left">
             <p>Rating</p>
             <img src="/assets/logo2.png" width="50" alt="" />
             <p>Review next</p>
           </div>
-          <div>
+          <div className="testimonials-right">
             <p>Name</p>
           </div>
         </div>
 
         <div className="testimonials-card">
-          <div>
+          <div className="testimonials-left">
             <p>Rating</p>
             <img src="/assets/logo2.png" width="50" alt="" />
             <p>Review next</p>
           </div>
-          <div>
+          <div className="testimonials-right">
             <p>Name</p>
           </div>
         </div>
